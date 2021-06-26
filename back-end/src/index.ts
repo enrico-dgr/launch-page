c59fb1178d62ac655a50ac4b5296d67bda35b533
enrico-dgr <elcidwar@gmail.com>
2021-06-26 22:13:41 +0200

    renamed and added imports to src/index

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,6 +1,25 @@
+import * as ElementHandle from './ElementHandle';
+import * as ErrorInfos from './ErrorInfos';
+import * as Json from './Json';
 import * as jsonFiles from './jsonFiles';
+import * as Page from './Page';
+import * as Puppeteer from './Puppeteer';
 import * as readline from './readline';
+import * as SettingsByLanguage from './SettingsByLanguage';
 import * as utils from './utils';
+import * as WebDeps from './WebDeps';
 import * as WebProgram from './WebProgram';
 
-export { WebProgram, utils, readline, jsonFiles };
+export {
+  WebProgram,
+  utils,
+  readline,
+  jsonFiles,
+  ElementHandle,
+  ErrorInfos,
+  Json,
+  Page,
+  WebDeps,
+  SettingsByLanguage,
+  Puppeteer,
+};
